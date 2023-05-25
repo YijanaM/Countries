bfc7b9b96c9db16b1df1e75e458a64cd98368c21
ysalazar <layija.16@hotmail.com>
2023-05-24 22:43:41 -0500

    Se agregan pruebas unitarias a las demás clases del proyecto

---
+++ src/test/java/com/test/bancolombia/contry/exceptions/ExceptionResponseTest.java
@@ -0,0 +1,22 @@
+package com.test.bancolombia.contry.exceptions;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class ExceptionResponseTest {
+
+    @Test
+    public void testGetMessage() {
+        String expectedMessage = "Test message";
+        ExceptionResponse exceptionResponse = new ExceptionResponse(expectedMessage);
+        String actualMessage = exceptionResponse.getMessage();
+        Assertions.assertEquals(expectedMessage, actualMessage);
+    }
+
+    @Test
+    public void testSetMessage() {
+        String expectedMessage = "Test message";
+        ExceptionResponse exceptionResponse = new ExceptionResponse("");
+        exceptionResponse.setMessage(expectedMessage);
+        String actualMessage = exceptionResponse.getMessage();
+        Assertions.assertEquals(expectedMessage, actualMessage);
+    }
+}
