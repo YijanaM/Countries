dc51b28d3326a1aa1af0584043a8239261c17c77
ysalazar <layija.16@hotmail.com>
2023-05-24 21:51:45 -0500

    Se agrega dependencia de Junit para pruebas unitarias y se realiza ajustes en servicio de pais

--- src/main/java/com/test/bancolombia/contry/Controllers/CountryController.java
+++ src/main/java/com/test/bancolombia/contry/Controllers/CountryController.java
@@ -1,9 +1,7 @@
 package com.test.bancolombia.contry.Controllers;
 
 import com.test.bancolombia.contry.models.Country;
-import com.test.bancolombia.contry.repositoies.LogRepository;
 import com.test.bancolombia.contry.services.CountryService;
-import com.test.bancolombia.contry.services.LogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,14 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.test.bancolombia.contry.exceptions.CountryNotFoundException;
+import com.test.bancolombia.contry.exceptions.ExceptionResponse;
 
 @RestController
 @RequestMapping("api/countries")
 public class CountryController {
-    @Autowired
-    private  LogRepository logRepository;
-    @Autowired
-    private  LogService logService;
+
     @Autowired
     private CountryService countryService;
 
@@ -29,18 +26,25 @@ public class CountryController {
     }
 
     @GetMapping(value = "/name/{name}")
-    public ResponseEntity<?> getCountryByName(@PathVariable String name){
-        Country country = countryService.getCountryByName(name);
+    public ResponseEntity<?> getCountryByName(@PathVariable String name) {
+        try{
+            Country country = (Country) countryService.getCountryByName(name);
 
-        if (country != null) {
-            Country countryInfo = new Country();
-            countryInfo.setArea(country.getArea());
-            countryInfo.setPopulation(country.getPopulation());
-            return ResponseEntity.ok(countryInfo);
-        }else{
-            String msg = "El país '" + name + "' no fue encontrado";
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(msg);
+            if (country != null) {
+                Country countryInfo = new Country();
+                countryInfo.setArea(country.getArea());
+                countryInfo.setPopulation(country.getPopulation());
+                return ResponseEntity.ok(countryInfo);
+            }else{
+                String msg = "El país '" + name + "' no fue encontrado";
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(msg);
+            }
+
+        }catch (CountryNotFoundException ex) {
+            ExceptionResponse exceptionResponse = new ExceptionResponse(ex.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionResponse);
         }
+
 
     }
 }
--- src/main/java/com/test/bancolombia/contry/services/CountryService.java
+++ src/main/java/com/test/bancolombia/contry/services/CountryService.java
@@ -7,14 +7,46 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.util.UriComponentsBuilder;
+
 @Service
 public class CountryService {
     @Autowired
     private  LogService logService;
-    @Value("https://restcountries.com/v3.1/all")
+    @Value("${api.url.all}")
     private String apiUrlAll;
     @Value("${api.url}")
     private String apiUrl;
+
+    public CountryService() {
+    }
+
+    public LogService getLogService() {
+        return logService;
+    }
+
+    public void setLogService(LogService logService) {
+        this.logService = logService;
+    }
+
+    public String getApiUrlAll() {
+        return apiUrlAll;
+    }
+
+    public void setApiUrlAll(String apiUrlAll) {
+        this.apiUrlAll = apiUrlAll;
+    }
+
+    public String getApiUrl() {
+        return apiUrl;
+    }
+
+    public void setApiUrl(String apiUrl) {
+        this.apiUrl = apiUrl;
+    }
 
     public String getCountries(){
         RestTemplate restTemplate = new RestTemplate();
@@ -23,20 +55,35 @@ public class CountryService {
         return response;
     }
 
-    public Country getCountryByName(@PathVariable String name){
+    public Object getCountryByName(@PathVariable String name){
         RestTemplate restTemplate = new RestTemplate();
-        String url = apiUrl + name + "?fullText=true";
+        Country[] countries = new Country[1];
+        Country countriesResponse= new Country();
+        String url = UriComponentsBuilder.fromUriString(apiUrl)
+                .pathSegment(name)
+                .queryParam("fullText", "true")
+                .toUriString();
 
         try {
             logService.saveLog(name, url);
-            Country[] countries = restTemplate.getForObject(url, Country[].class);
+            //Country[] countries = restTemplate.getForObject(url, Country[].class);
 
-            if (countries != null && countries.length > 0) {
-                return countries[0];
+            ResponseEntity<Country[]> response = restTemplate.exchange(url, HttpMethod.GET, null, Country[].class);
+
+            if (response.getStatusCode() == HttpStatus.OK) {
+                countries = response.getBody();
+                if (countries != null && countries.length > 0) {
+                    countriesResponse =countries[0];
+                }
             }
-        }catch (HttpClientErrorException.NotFound ex){
-            System.err.println(ex.getMessage());
+        } catch (HttpClientErrorException.NotFound ex) {
+            ex.printStackTrace();
+            countriesResponse = null;
+        } catch (Exception e){
+            e.printStackTrace();
+            countriesResponse = null;
         }
-        return null;
+        return countriesResponse;
+
     }
 }
